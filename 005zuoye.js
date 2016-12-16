4cb5f1893e8663879fe704accf8dba0196d4edf0
yangyangshan <1059525934@qq.com>
2016-12-16 19:41:40 +0800

    update 005

---
+++ 005zuoye.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>005</title>
+	<link rel="stylesheet"  href="005zuoye.css">
+</head>
+<body>
+	<div class="banner">
+		<span>标题</span><em>栏</em>
+		<div class="bannera">
+		<a>糯米</a><a>新闻</a><a>hao123</a><a>地图</a><a>视频</a><a>贴吧</a><a>设置</a><a>关于我们</a>
+		</div>
+	</div>
+	<div style="clear:both;"></div>
+	<div class="topnav">
+		<span>关于</span><em>我们</em>
+		<div class="topnava">
+		<a>家庭</a><i>/</i><a>设置</a><i>/</i><a>关于</a>
+		</div>
+	</div>
+	<div style="clear:both;"></div>
+	<script src="005zuoye.js"></script>
+</body>
+</html>
